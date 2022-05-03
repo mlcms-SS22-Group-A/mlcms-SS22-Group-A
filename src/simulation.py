5c8c7f891fb16648a8b6ff4be73a03a9dd897a0e
Arda Yazgan <arda_yazgan@hotmail.com>
2022-05-03 16:49:08 +0300

    Changed evaluation function to use dijkstra and started adding pedestrian interaction

--- src/simulation.py
+++ src/simulation.py
@@ -1,65 +1,56 @@
 import math
-import numpy as np
+
+from src.dijkstra import *
+
+max_distance = 1.0
+
 
 class Simulator:
-    
-    def __init__(self):
-        self.target=False
-    def pedestrianupdate(self,matrix, row, column, trow, tcolumn):
+
+    def __init__(self, matrix, target):
+        self.target = target
+        self.distances = dijkstra(matrix, target)
+
+    def pedestrianupdate(self, matrix, row, column, pedestrians):
         """
         This function serves as a helper to the update() function (see below) to determine the state of cellular
         automaton at next time step, given the current state, position of pedestrian and target.
 
-        :param matrix: Current state of cellular automaton
+        :param matrix: Current state of cellular automata
         :param row: Current row of pedestrian
         :param column: Current column of pedestrian
-        :param trow: The row at which target is stored
-        :param tcolumn: The column at which target is stored
+        :param pedestrians: List of all pedestrians
         """
-        neighbours = []
-        m_row, m_col = matrix.shape
-        if row != 0:
-            neighbours.append([matrix[row - 1][column], row - 1, column])
-        if row != m_row - 1:
-            neighbours.append([matrix[row + 1][column], row + 1, column])
-        if column != 0:
-            neighbours.append([matrix[row][column - 1], row, column - 1])
-        if column != m_col - 1:
-            neighbours.append([matrix[row][column + 1], row, column + 1])
+        neighbours = get_neighbours(self.distances, row, column, False)
 
         for i in range(0, len(neighbours)):
-            if neighbours[i][0] == 3:
-                if not self.target:
-                      matrix[row][column]=0  
-                self.target=True
-                return 
+            if neighbours[i][0] == 0.0:
+                matrix[row][column] = 0
+                return
 
-        distances = []
+        neighbours.append([self.distances[row][column], row, column])
         for i in range(0, len(neighbours)):
-            distances.append(math.sqrt((neighbours[i][1] - trow) ** 2 + (neighbours[i][2] - tcolumn) ** 2))
+            for j in range(0, len(pedestrians)):
+                if pedestrians[j][0] == row and pedestrians[j][1] == column:
+                    continue
+                distance = math.sqrt(
+                    (neighbours[i][1] - pedestrians[j][0]) ** 2 + (neighbours[i][2] - pedestrians[j][1]) ** 2)
+                if distance < max_distance:
+                    neighbours[i][0] += math.exp(1 / (distance ** 2 - max_distance ** 2))
 
-        min_distance_index = np.array(distances).argmin()
-        minNeighbor =  matrix[neighbours[min_distance_index][1]][neighbours[min_distance_index][2]]
-        while minNeighbor == 2:
-            distances[min_distance_index] =distances[np.array(distances).argmax()]+1
-            min_distance_index = np.array(distances).argmin()
-            minNeighbor =  matrix[neighbours[min_distance_index][1]][neighbours[min_distance_index][2]]
-        if minNeighbor == 1:
-            return
+        neighbours_distance = list(map(lambda n: n[0], neighbours))
+        min_distance_index = np.array(neighbours_distance).argmin()
+
         matrix[row][column] = 0
         matrix[neighbours[min_distance_index][1]][neighbours[min_distance_index][2]] = 1
-    
 
-    def update(self,matrix, pedestrians, trow, tcolumn):
+    def update(self, matrix, pedestrians):
         """
          This function updates the state of cellular automaton such that all pedestrians move towards the target.
 
-         :param matrix: Current state of cellular automaton
+         :param matrix: Current state of cellular automata
          :param pedestrians: List of pedestrians
-         :param trow: The row at which target is stored
-         :param tcolumn: The column at which target is stored
        """
         row, _ = pedestrians.shape
         for i in range(0, row):
-             self.pedestrianupdate(matrix, pedestrians[i][0], pedestrians[i][1], trow, tcolumn)
-       
+            self.pedestrianupdate(matrix, pedestrians[i][0], pedestrians[i][1], pedestrians)
