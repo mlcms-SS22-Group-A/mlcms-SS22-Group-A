9d022556ef1c0dd4ec4bfbf57711af7ab2dce048
Atamert Rahma <atamertiel@gmail.com>
2022-05-19 14:20:07 +0200

    add ped added, task3 ped times added

--- add_pedestrian.py
+++ add_pedestrian.py
@@ -29,18 +29,36 @@ def read_opts():
 def place_ped(file_location, x_location, y_location):
     with open(file_location, 'r') as f_read:
         scenario_dict = json.load(f_read)
-    f_read.close()
+    targetId = scenario_dict["scenario"]["topography"]["targets"][0]["id"]
 
-    with open("pedestrian.json", 'w') as f_read:
+    topography_ids = []
+    for obstacle in scenario_dict["scenario"]["topography"]["obstacles"]:
+        topography_ids.append(obstacle["id"])
+    for target in scenario_dict["scenario"]["topography"]["targets"]:
+        topography_ids.append(target["id"])
+    for source in scenario_dict["scenario"]["topography"]["sources"]:
+        topography_ids.append(source["id"])
+    for ped in scenario_dict["scenario"]["topography"]["dynamicElements"]:
+        topography_ids.append(ped["attributes"]["id"])
+
+    topography_ids.sort()
+    numItems = len(topography_ids)
+    pedId = topography_ids[numItems - 1] + 1
+
+    with open("pedestrian.json", 'r+') as f_read:
         ped = json.load(f_read)
+
     f_read.close()
 
+    ped["attributes"]["id"] = pedId
     ped["position"]["x"] = float(x_location)
     ped["position"]["y"] = float(y_location)
+    ped["targetIds"] = [ int(targetId) ]
 
     scenario_dict["scenario"]["topography"]["dynamicElements"].append(ped)
 
-    with open("Sample.scenario", "w") as outfile:
+    f_outputName = file_location.replace(".scenario", "_pedAdded.scenario")
+    with open(f_outputName, "w") as outfile:
         json.dump(scenario_dict, outfile, indent=2)
     outfile.close()
 
