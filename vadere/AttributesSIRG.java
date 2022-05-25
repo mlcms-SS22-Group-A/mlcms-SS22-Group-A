bc3887ec0b0b9a8fc4524a53cdbdc7aa9a2d2ea4
Arda Yazgan <arda_yazgan@hotmail.com>
2022-05-25 17:55:49 +0300

    Finalize Exercise-2

---
+++ vadere/AttributesSIRG.java
@@ -0,0 +1,24 @@
+package org.vadere.state.attributes.models;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.vadere.annotation.factories.attributes.ModelAttributeClass;
+import org.vadere.state.attributes.Attributes;
+
+@ModelAttributeClass
+public class AttributesSIRG extends Attributes {
+
+	private int infectionsAtStart = 0;
+	private double infectionRate = 0.01;
+	private double recoveryRate = 0.02;
+	private double infectionMaxDistance = 1;
+
+	public int getInfectionsAtStart() { return infectionsAtStart; }
+	public double getInfectionRate() { return infectionRate; }
+	public double getRecoveryRate() { return recoveryRate; }
+	public double getInfectionMaxDistance() {
+		return infectionMaxDistance;
+	}
+	public void setInfectionMaxDistance(double infectionMaxDistance) { this.infectionMaxDistance = infectionMaxDistance; }
+}
---
+++ vadere/sir/SIRType.java
@@ -0,0 +1,10 @@
+package org.vadere.simulator.models.groups.sir;
+
+public enum SIRType
+{
+    ID_INFECTED,
+    ID_SUSCEPTIBLE,
+    ID_REMOVED,
+    // new type added as recovered
+    ID_RECOVERED
+}
