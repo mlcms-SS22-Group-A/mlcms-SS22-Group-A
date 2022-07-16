60eec6c39bf8dc21a36f46b19a869f1a4d72dcac
Arda Yazgan <ge25zuj@mytum.de>
2022-07-16 13:58:49 +0300

    Dataloaders and trainer

--- Euler.py
+++ Euler.py
@@ -4,19 +4,29 @@ import pytorch_lightning as pl
 
 class Euler(pl.LightningModule):
     def __init__(self, hparams):
-        super().__init__(hparams)
-        self.model = None
+        super().__init__()
+        self.save_hyperparameters(hparams)
+        self.model = torch.nn.Sequential(
+            torch.nn.Linear(2, 32),
+            torch.nn.LeakyReLU(),
+            torch.nn.Linear(32, 32),
+            torch.nn.LeakyReLU(),
+            torch.nn.Linear(32, 32),
+            torch.nn.LeakyReLU(),
+            torch.nn.Linear(32, 2),
+            torch.nn.LeakyReLU(),
+        )
 
     def forward(self, x):
         return x + self.hparams["delta_t"] * self.model(x)
 
     def training_step(self, batch):
-        x, y = batch
+        x, x_target = batch
 
         pred = self.forward(x)
 
         loss_fn = torch.nn.MSELoss()
-        return loss_fn(pred, y)
+        return loss_fn(pred, x_target)
 
     def configure_optimizers(self):
         return torch.optim.Adam(self.model.parameters())
